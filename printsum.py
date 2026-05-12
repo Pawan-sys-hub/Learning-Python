58467d240835a4600b655f1bba4aab422dd84419
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-12 22:46:00 +0545

    add sub multiply and division

--- printsum.py
+++ printsum.py
@@ -1,4 +1,10 @@
 a = 4
 b = 5
 sum = a + b
+difference = a - b 
+multiply = a * b
+division = a / b
 print ("sum:", sum) 
+print ("difference:", difference)
+print ("multiply:", multiply)
+print ("dividion:", division)
