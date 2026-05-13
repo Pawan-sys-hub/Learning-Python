61c5f83b86ff72a9c86218a9927d39e43885caed
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-13 06:41:29 +0545

    adding

--- printsum.py
+++ printsum.py
@@ -1,10 +1,13 @@
 a = 4
 b = 5
+c = 9
 sum = a + b
 difference = a - b 
 multiply = a * b
 division = a / b
+sumanddiff = a + b -c
 print ("sum:", sum) 
 print ("difference:", difference)
 print ("multiply:", multiply)
 print ("dividion:", division)
+print ("sumanddiff:", sumanddiff)
