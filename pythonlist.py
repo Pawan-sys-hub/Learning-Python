e5270b1e88f28ca1338e64326214c23f053b5512
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-19 13:36:51 +0545

    list of python

---
+++ pythonlist.py
@@ -0,0 +1,18 @@
+# list of python 
+
+hei = 10
+bre = 20
+len = 15
+
+area = hei * bre * len #multiplication
+
+print(type(area)) #type of area
+
+print("Area of my room is :", area )
+
+room = [["heignt",hei],
+        ["breth",bre],
+        ["length",len]]
+
+print(room)
+
