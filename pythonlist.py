d3b4740cbb55c9c0dd9b215b39c0d497d219c8d8
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-19 22:56:58 +0545

    lists

--- pythonlist.py
+++ pythonlist.py
@@ -48,3 +48,17 @@ house = [["hallway", 11.25],
 
 
 house[4][1]
+
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Delete the poolhouse items from the list
+
+del areas[10]
+del areas[10]
+# Print  updated list
+
+print(areas)
+
