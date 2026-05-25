95008f616d2c54dde8d0e39f94e4d0dc74f574cb
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-05-25 22:52:33 +0545

    pratice

--- firstprogram.py
+++ firstprogram.py
@@ -1,1 +1,21 @@
 print("hello world")
+
+#inputs and output of puthon
+
+Name = input("Enter your name:")
+
+Age = input("Enter Your age:")
+
+print("My name is " ,Name + ".")
+print("My age is:" ,Age + ".")
+print (f"My name is {Name}.My age is {Age}.")
+
+
+#if statement 
+age = 15
+if age >= 18:
+  print("Audilt")
+else:
+  print("minor")
+
+
