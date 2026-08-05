ab93685bc87ea925a848c2c61f87501f2b1571c7
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-08-05 17:18:17 +0545

    operators

--- Learnings/firstprogram.py
+++ Learnings/firstprogram.py
@@ -18,4 +18,27 @@ if age >= 18:
 else:
   print("minor")
 
+print("hello world i am back")
 
+Names = input("enter your college name")
+Address = input ("Enter your college address")
+Class = input ("your claass in you study")
+
+print ("Name of your college is" ,Names + ".")
+
+print (" your college is in" ,Address + ".")
+
+print ("Name of your class is" ,Class + ".")
+
+print (f"my college name is {Names}. My College lies in {Address} and i study in {Class}")
+
+
+
+a = input.int("enter the first number =")
+b = input.int("enter the second number =")
+
+sum = a + b
+difference = a * b
+
+print("sum",sum)
+print("difference is =", difference)
---
+++ Learnings/pandas.py
@@ -0,0 +1,8 @@
+
+
+a = int(input("enter the first number ="))
+b = int(input("enter the second number ="))
+sum = a + b
+difference = a * b
+print("sum",sum)
+print("difference is =", difference)
