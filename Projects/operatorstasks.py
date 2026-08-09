38f11c961330aafebbb956bdbe570f6e51ba6d21
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-08-09 19:41:07 +0545

    lists

---
+++ Projects/operatorstasks.py
@@ -0,0 +1,13 @@
+#print 1 to 100
+
+#for i in range (1,100):
+#  print(i)
+
+# print the even numbers
+
+even = (int(input("Enter the number:")))
+
+if even%2 == 0:
+  print("It is even number")
+else:
+  print("odd")
