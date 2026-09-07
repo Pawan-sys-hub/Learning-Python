ff57f9abc084342d172a9188cf8af4dde7c5d1d3
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-09-07 07:51:50 +0545

    marks

--- Learnings/hello.py
+++ Learnings/hello.py
@@ -1,1 +1,20 @@
-print("problem solving")
+print("problem solving of students")
+name = input("Enter your name :")
+marks = []
+number_of_subject = 5
+
+#input
+for i in range (number_of_subject):
+  mark = float(input(f"Enter the marks for subjects {i + 1}:"))
+  marks.append(mark)
+
+#calculation 
+
+total_marks = sum(marks)
+average_marks = total_marks / number_of_subject 
+percentage = total_marks / 500 * 100
+
+#output 
+print(f"sum of total marks {total_marks} " )
+print(f"average marks : {average_marks}")
+print(f"percentage: {percentage}")
