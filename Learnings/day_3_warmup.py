0b90e4a8f0f736f2d02c3dc2427bd74001299d3a
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-09-08 22:57:45 +0545

    another done

--- Learnings/day_3_warmup.py
+++ Learnings/day_3_warmup.py
@@ -7,3 +7,9 @@ largest = max(numbers)
 print(f"The largest number is - {largest}")
 smallest = min(numbers)
 print(f"The smallest number is {smallest}")
+
+add = sum(numbers)
+print(f"the sum of number is -- {add}")
+
+average = add / 6 
+print(f"average of the sum of number is -- {average}")
