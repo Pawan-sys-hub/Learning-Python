03e44ceed0ea591acda7a4044269eead92f1ef73
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-09-08 22:53:21 +0545

    my first challange

---
+++ Learnings/day_3_warmup.py
@@ -0,0 +1,9 @@
+print("Challange 1")
+
+numbers = [12,23,43,123,1212,121]
+
+print(numbers.append(34))
+largest = max(numbers)
+print(f"The largest number is - {largest}")
+smallest = min(numbers)
+print(f"The smallest number is {smallest}")
