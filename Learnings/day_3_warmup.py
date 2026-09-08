272b92772502a04c850d3579321b3ca1f98484fc
Pawan Pokharel <pawanpokharel523@gmail.com>
2026-09-08 23:27:00 +0545

    challenge 2

--- Learnings/day_3_warmup.py
+++ Learnings/day_3_warmup.py
@@ -25,3 +25,40 @@ print(even)
 
 odd = [ o for o in numbers if o % 3 == 0]
 print(odd)
+
+
+print("Challange 2")
+
+marks = [45,54,64,98,90,12,56]
+
+total_marks = sum(marks)
+print(f"The sum of number is {total_marks}")
+average = total_marks / 2
+print(f"the average marks obtain by the student is {average}")
+
+highest_marks = max(marks)
+print(f"the highest marks obtain my student is {highest_marks}")
+
+lowest_marks = min(marks)
+print(f"the lowest marks obtain by the student is {lowest_marks}")
+
+print("using loops")
+
+total = 0
+highest_mark = marks[0]
+lowest_mark = marks [0]
+above_80 = 0
+
+for m in marks:
+  total += m
+  if m > highest_mark:
+    highest_mark = m
+  if m < lowest_mark:
+    lowest_mark = m
+  if m > 80:
+    above_80 += 1
+averages = total / len(marks)
+print (f"Average {averages}")
+print(f"highest number {highest_mark}")
+print(f"lowest nmber is {lowest_mark}")
+print(f"Marks above 80 is {above_80}")
